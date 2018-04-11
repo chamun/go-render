d366846eba6851f763e45f4527870ef4ce1fd559
Rodrigo Chamun <rochamun@gmail.com>
2018-04-11 20:58:51 -0300

    Move light types to lights.go

--- scene.go
+++ scene.go
@@ -73,40 +73,6 @@ func (r *Ray) IntersectSphere(s Sphere) (float64, float64) {
 	return t1, t2
 }
 
-type Light interface {
-	// ComputeLight calculates the total brightness of point p given its normal
-	// vector n.
-	ComputeLight(point, normal Vector) float64
-}
-
-type DirectionalLight struct {
-	Intensity float64
-	Direction Vector
-}
-
-func (light DirectionalLight) ComputeLight(_, normal Vector) float64 {
-	cos := normal.Cos(light.Direction)
-	return math.Max(light.Intensity*cos, 0)
-}
-
-type PointLight struct {
-	Intensity float64
-	Position  Vector
-}
-
-func (light PointLight) ComputeLight(point, normal Vector) float64 {
-	cos := normal.Cos(light.Position.Minus(point))
-	return math.Max(light.Intensity*cos, 0)
-}
-
-type AmbientLight struct {
-	Intensity float64
-}
-
-func (light AmbientLight) ComputeLight(_, _ Vector) float64 {
-	return light.Intensity
-}
-
 type Scene struct {
 	spheres []Sphere
 	bgColor Color
